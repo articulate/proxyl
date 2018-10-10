51b53a7de718098c28a3e9bd6d309e5a70d4786f
Scott McCormack <scott.mccormack@articulate.com>
2018-10-10 11:10:46 -0400

    Actually works

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ const { compose } = require('ramda')
 const http = require('http')
 const { mount } = require('paperplane')
 
-const logger = require('./logger')
 const requestToEvent = require('./requestToEvent')
 
-const [ path, name ] = process.argv0.split('.')
+const [ path, name ] = process.argv[2].split('.')
+console.log(`Loading handler \`${name}\` from ${path}.js`)
+
 const handlerPath = require.resolve(path, { paths: [ process.cwd() ] })
 const handler = require(handlerPath)[name]
 
 const app = compose(handler, requestToEvent)
+const noop = Function.prototype
 
-http.createServer(mount({ app, logger }))
+http.createServer(mount({ app, cry: noop, logger: noop })).listen(3000)
--- src/logger.js
+++
@@ -1,7 +0,0 @@
-const { compose, dissocPath } = require('ramda')
-const paperplane = require('paperplane')
-
-const logger =
-  compose(paperplane.logger, dissocPath(['req', 'headers']))
-
-module.exports = logger
