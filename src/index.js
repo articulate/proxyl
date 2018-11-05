449cc56b95b8d104c829f5f6488a81ad6e658483
Scott McCormack <scott.mccormack@articulate.com>
2018-11-05 12:18:29 -0500

    Safely default the authorizer

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const resolve =
 
 program
   .usage('[options] path/to/file.handlerName')
-  .option('-a, --auth <authorizer>', 'path/to/authorizer.handlerName')
+  .option('-a, --auth <authorizer>', 'path/to/authorizer.handlerName', '')
   .option('-p, --port <port>', 'Port to listen on', 3000)
   .option('-w, --watch <path>', 'Path(s) to watch, defaults to pwd', append, [])
   .parse(process.argv)
